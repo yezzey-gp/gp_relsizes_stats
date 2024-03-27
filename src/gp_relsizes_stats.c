bf8131085d30b8daa5bd898912a020e5df30dcb5
Andrey Ovchinnikov <ch1nn1y@qavm-c1d7029f.qemu>
2024-03-27 14:40:41 +0300

    save commit; add functions for segments

--- src/gp_relsizes_stats.c
+++ src/gp_relsizes_stats.c
@@ -6,6 +6,7 @@
 #include "access/xact.h"
 #include "catalog/pg_foreign_table.h"
 #include "catalog/pg_type.h"
+#include "cdb/cdbvars.h"
 #include "commands/copy.h"
 #include "commands/defrem.h"
 #include "commands/explain.h"
@@ -25,6 +26,7 @@
 #include "optimizer/planmain.h"
 #include "optimizer/restrictinfo.h"
 #include "optimizer/var.h"
+#include "pgstat.h"
 #include "utils/builtins.h"
 #include "utils/elog.h"
 #include "utils/lsyscache.h"
@@ -32,19 +34,33 @@
 #include "utils/rel.h"
 #include "utils/timestamp.h"
 
+
+#include <limits.h> 
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 PG_MODULE_MAGIC;
 
 PG_FUNCTION_INFO_V1(int_square_c_impl);
 PG_FUNCTION_INFO_V1(collect_table_size);
 
+typedef struct segment_request_data {
+    Datum *contents;
+    Datum *hostnames;
+    Datum *ports;
+    Datum *datadirs;
+} segment_request_data_t;
+
 void _PG_init(void);
 void _PG_fini(void);
 static List* get_collectable_db_ids(List *ignored_db_names);
 static int create_trunkate_table();
-static void load_all_segments_stats(char *base_dir, int dboid, TimestampTz start_time);
+static segment_request_data_t* get_segments_basedirs();
+static void load_segment_files(char *path, char* csv_path, int segment);
+static void load_all_segments_stats(char *dest_dir, int dboid);
+
 Datum collect_table_size(PG_FUNCTION_ARGS);
 
 static List* get_collectable_db_ids(List *ignored_db_names) {
@@ -150,11 +166,180 @@ finish_SPI:
     return retcode;
 }
 
-static void load_all_segments_stats(char *base_dir, int dboid, TimestampTz start_time) {
+static segment_request_data_t* get_segments_basedirs() {
+    int retcode = 0;
+    segment_request_data_t *packed_data = NULL;
+    char *sql = "SELECT content, hostname, port, datadir \
+                FROM gp_segment_configuration \
+                WHERE role = 'p' \
+                ORDER BY port"; 
+    /* connect to SPI */
+    retcode = SPI_connect();
+    if (retcode < 0) { /* error */
+        elog(ERROR, "get_segments_basedirs: SPI_connect returned %d", retcode);
+        goto finish_SPI;
+    } 
+
+    /* execute sql query to get table */
+    retcode = SPI_execute(sql, true, 0);
+
+    /* check errors if they're occured during execution */
+    if (retcode != SPI_OK_SELECT || SPI_processed < 0) {
+        elog(ERROR, "get_segments_basedirs: SPI_execute returned %d, processed %lu rows", retcode, SPI_processed);
+        goto finish_SPI;
+    }
+    
+    packed_data = palloc0(sizeof(*packed_data));
+    packed_data->contents = palloc0(SPI_processed * sizeof(*packed_data->contents));
+    packed_data->hostnames = palloc0(SPI_processed * sizeof(*packed_data->hostnames));
+    packed_data->ports = palloc0(SPI_processed * sizeof(*packed_data->ports));
+    packed_data->datadirs = palloc0(SPI_processed * sizeof(*packed_data->datadirs));
+
+    Datum *tuple_values = palloc0(SPI_tuptable->tupdesc->natts * sizeof(*tuple_values));
+    bool *tuple_nullable = palloc0(SPI_tuptable->tupdesc->natts * sizeof(*tuple_nullable));
+    
+    for (int i = 0; i < SPI_processed; ++i) {
+        HeapTuple current_tuple = SPI_tuptable->vals[i];
+        heap_deform_tuple(current_tuple, SPI_tuptable->tupdesc, tuple_values, tuple_nullable);
+
+        packed_data->contents[i] = tuple_values[0];
+        packed_data->hostnames[i] = tuple_values[1];
+        packed_data->ports[i] = tuple_values[2];
+        packed_data->datadirs[i] = tuple_values[3];
+    }
+
+    pfree(tuple_values);
+    pfree(tuple_nullable);
+
+finish_SPI:
+    /* finish SPI */
+    SPI_finish();
+    return packed_data;
+}
+
+static void clear_dest_dir(char *dest_path) {
+    struct stat stb;
+    struct dirent *file;
+    char new_path[PATH_MAX];
+    DIR *csv_dir = AllocateDir(dest_path);
+    if (!csv_dir) {
+        return;
+    }
+    while ((file = ReadDir(csv_dir, dest_path)) != NULL) {
+        char *filename = file->d_name; 
+        /* if limit of PATH_MAX reached skip file */
+        if (sprintf(new_path, "%s/%s", dest_path, filename) >= sizeof(new_path)) {
+            continue;
+        }
+
+        /* do lstat if returned error => continue */
+        if (lstat(new_path, &stb) < 0 && S_ISREG(stb.st_mode) && 
+                (strncmp(filename, "files_", 6) == 0 || strncmp(filename, "map_", 4) == 0)) { // TODO more clear way to check prefix?
+            /* remove file with old csv data */
+            remove(new_path);
+        }
+    }
+    FreeDir(csv_dir);
+}
+
+static void load_segment_files(char *path, char* csv_path) {
+    /* if {path} is NULL => return */
+    if (!path) {
+        return;
+    }
+
+    char new_path[PATH_MAX];
+    DIR *current_dir = AllocateDir(path);
+    /* if {current_dir} did not opened => return */
+    if (!current_dir) {
+        return;
+    }
+
+    struct dirent *file;
+    /* start itterating in {current_dir} */
+    while ((file = ReadDir(current_dir, path)) != NULL) {
+        char *filename = file->d_name;
+        /* if filename is special as "." or ".." => continue */
+        if (strcmp(filename, ".") == 0 || strcmp(filename, "..") == 0) {
+            continue;
+        }
+
+        /* if limit of PATH_MAX reached skip file */
+        if (sprintf(new_path, "%s/%s", path, filename) >= sizeof(new_path)) {
+            continue;
+        }
+
+        struct stat stb;
+        /* do lstat if returned error => continue */
+        if (lstat(new_path, &stb) < 0) {
+            continue;
+        }
+
+        if (S_ISREG(stb.st_mode)) {
+            // here write to csv data about file
+            // need to write ({start_time}, {segment}, {filename_numbers}, {filename}, {stb.st_size}, {stb.st_mtimespec})
+            // can store a lot of different stats here (lstat sys call is MVP)
+            FILE *csv_ptr = AllocateFile(csv_path, "a");
+            fprintf(csv_ptr, "%d,%s,%ld,%ld\n", GpIdentity.segindex, filename,
+                    stb.st_size, stb.st_mtime);
+            FreeFile(csv_ptr);
+        } else if (S_ISDIR(stb.st_mode)) {
+            load_segment_files(new_path, csv_path, segment);
+        }
+    }
+    FreeDir(current_dir);
+}
+
+static void load_segment_map(Datum host, Datum port) {
     return;
 }
 
-static int write_collected_data(char *base_dir, int dboid) {
+static void load_segment_stats(int dboid, Datum segment, Datum host, Datum port, Datum base_dir, char *dest_dir) {
+    char filepath_files[1000]; // TODO put 1000 in constant
+    char filepath_map[1000]; // TODO put 1000 in constant
+    char final_base_dir[1000]; // TODO put correct value in constant and change 1000 to it
+    
+    /* create csv files and put headers in them */
+    sprintf(filepath_files, "%s/files_gpseg%d.csv", dest_dir, DatumGetInt32(segment));
+    FILE *csv_ptr = AllocateFile(filepath_files, "w");
+    fprintf(csv_ptr, "startTime, segment, filename, size, lastModified\n");
+    FreeFile(csv_ptr);
+
+    sprintf(filepath_map, "%s/map_gpseg%d.csv", dest_dir, DatumGetInt32(segment));
+    csv_ptr = AllocateFile(filepath_map, "w");
+    fprintf(csv_ptr, "\n"); // TODO
+    FreeFile(csv_ptr);
+
+    // TODO prepare correct base dir "{base_dir}/base/{dboid}"
+    sprintf(final_base_dir, "%s/base/%d", DatumGetCString(base_dir), dboid);
+    
+    TimestampTz start_time = GetCurrentTimestamp();
+    load_segment_files(final_base_dir, filepath_files, DatumGetInt32(segment));
+    load_segment_map(segment, host, port);
+    TimestampTz finish_time = GetCurrentTimestamp();
+}
+
+
+static void load_all_segments_stats(char *dest_dir, int dboid) {
+    // get segment basedirs
+    segment_request_data_t *segment_data = get_segments_basedirs();
+
+    // clear segment base dir from old csv
+    clear_dest_dir(dest_dir);
+
+    int segments_cnt = sizeof(segment_data->contents);
+    // itterate in segment base_dirs + segment names
+    for (int i = 0; i < segments_cnt; ++i) {
+        // starts load_segment_stats(_ssh??) function
+        load_segment_stats(dboid, segment_data->contents[i], segment_data->hostnames[i], segment_data->ports[i],
+                segment_data->datadirs[i], dest_dir);
+    }
+}
+
+static int write_collected_data(char *path, int dboid) {
+    if (!path) {
+        return 0;
+    }
     return 0;
 } 
 
@@ -173,7 +358,6 @@ Datum collect_table_size(PG_FUNCTION_ARGS) {
     List *ignored_db_names = NIL, *databases_ids;
     ListCell *current_cell;
     Oid elem_type;
-    TimestampTz collecting_start_time;
 
     // put all ignored_db names from fisrt array-argument
     ignored_db_names_array = PG_GETARG_ARRAYTYPE_P(0);
@@ -197,8 +381,8 @@ Datum collect_table_size(PG_FUNCTION_ARGS) {
     
     foreach(current_cell, databases_ids) {
         dboid = lfirst_int(current_cell); 
-        collecting_start_time = GetCurrentTimestamp();
-        load_all_segments_stats(base_dir, dboid, collecting_start_time);
+        // prepare csv
+        load_all_segments_stats(base_dir, dboid);
 
         retcode = write_collected_data(base_dir, dboid);
         if (retcode < 0) {
