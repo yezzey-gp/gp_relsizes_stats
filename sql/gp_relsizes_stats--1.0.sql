99a5af012b144245b573be1e1aa2eec22bf70e03
Andrey Ovchinnikov <ch1nn1y@qavm-c1d7029f.qemu>
2024-04-03 16:00:43 +0300

    write function for creating and truncating special info tables

--- sql/gp_relsizes_stats--1.0.sql
+++ sql/gp_relsizes_stats--1.0.sql
@@ -7,6 +7,27 @@
 
 -- Here go any C or PL/SQL functions, table or view definitions etc
 -- for example:
+CREATE FUNCTION prepare_info_tables() RETURNS VOID
+LANGUAGE plpgsql VOLATILE EXECUTE ON MASTER AS
+$func$
+BEGIN
+    -- create table, clear if it's exists and fill with actual data
+    EXECUTE 'CREATE TABLE IF NOT EXISTS gp_toolkit.segment_file_map
+        (segment INTEGER, reloid OID, relfilenode OID) 
+        WITH (appendonly=true) DISTRIBUTED BY (segment)';
+    EXECUTE 'TRUNCATE TABLE gp_toolkit.segment_file_map';
+    EXECUTE 'INSERT INTO gp_toolkit.segment_file_map 
+        SELECT gp_segment_id, oid, relfilenode FROM gp_dist_random(''pg_class'')';
+
+    -- create table and clear if it's exists 
+    EXECUTE 'CREATE TABLE IF NOT EXISTS gp_toolkit.segment_file_sizes
+        (segment INTEGER, relfilenode OID, filepath TEXT, size BIGINT, mtime BIGINT)
+        WITH (appendonly=true, OIDS=FALSE) DISTRIBUTED RANDOMLY';
+    EXECUTE 'TRUNCATE TABLE gp_toolkit.segment_file_sizes';
+END
+$func$;
+
+
 CREATE FUNCTION get_file_sizes_for_database(dboid INTEGER)
 RETURNS TABLE (segment INTEGER, relfilenode OID, filepath TEXT, size BIGINT, mtime BIGINT)
 AS 'MODULE_PATHNAME', 'get_file_sizes_for_database'
@@ -16,3 +37,4 @@ CREATE FUNCTION collect_table_sizes(ignored_dbnames VARCHAR[])
 RETURNS void
 AS 'MODULE_PATHNAME', 'collect_table_sizes'
 LANGUAGE C STRICT EXECUTE ON MASTER;
+
